c7e8b137750463185309f519a3ee878178be7450
fayiz kiliyamannil <fayiz.kiliyamannil@gmail.com>
2024-06-24 15:55:35 +0530

    bug fix

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express()
 const bodyParser = require('body-parser')
 require('dotenv').config()
 
-
 const mongoose = require('mongoose');
 const { text } = require('body-parser');
 mongoose.connect(process.env.MONGOD)
@@ -13,17 +12,15 @@ mongoose.connect(process.env.MONGOD)
 
 app.use(
   cors({
-    origin: 'https://sentiment-project1.vercel.app',
+    origin: process.env.CORS,
     methods: ['GET', "POST", "DELETE", "PUT", "PATCH"],
     credentials: true
   })
-)
+) 
 
 app.use(express.json()); // Parse JSON bodies, if any
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
 
 
 
